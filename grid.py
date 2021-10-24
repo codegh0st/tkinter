8863a0cb4cee8c0646b9d6330fe3babf131c8f85
codegh0st <foxycodex@gmail.com>
2021-10-24 10:50:27 +0530

    trying to push wrong code to wrong origin-> grid.py is different code but pushing it changes to already set orgin for clock.git

---
+++ grid.py
@@ -0,0 +1,18 @@
+from tkinter import *
+root = Tk()
+#creating a label widget
+mylabel = Label(root, text="Hi tkinter window ")
+mylabel1 = Label(root, text="Hi tkinter window ")
+mylabel2 = Label(root, text="Hi tkinter window ")
+
+
+mylabel.grid(row=0, column=0)
+mylabel1.grid(row=1, column=0)
+mylabel2.grid(row=2, column=0)
+
+# OR you can use this syntax will do the same thing. 
+# mylabel = Label(root, text="Hi tkinter window ").grid(row=0, column=0)
+# mylabel1 = Label(root, text="Hi tkinter window ").grid(row=1, column=0)
+# mylabel2 = Label(root, text="Hi tkinter window ").grid(row=1, column=0)
+
+root.mainloop()
